e4f63150eb78f67dede51b96099612153a33c669
Gabriel Alabi <Rexes_Games@yahoo.com>
2023-06-14 22:33:53 +0100

    start implementing data persistence

---
+++ SOQETDevelopmentAndExample/Assets/SOQET/Data Persistence/SaveAndLoad.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using SOQET.Others;
+
+namespace SOQET.DataPersistence
+{
+    public static class SaveAndLoad
+    {
+        //Save
+        public static void Save(Story dataToSave)
+        {
+
+        }
+
+        //Load
+        public static Story Load()
+        {
+            Story story = null;
+            return story;
+        }
+    }
+}
