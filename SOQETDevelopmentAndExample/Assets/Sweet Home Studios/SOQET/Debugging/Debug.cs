e775169c6f8d670052d6225511c378723bb4660b
Gabriel Alabi <Rexes_Games@yahoo.com>
2023-08-16 13:13:43 +0100

    Document Debugging SOQET Feature

--- SOQETDevelopmentAndExample/Assets/Sweet Home Studios/SOQET/Debugging/Debug.cs
+++ SOQETDevelopmentAndExample/Assets/Sweet Home Studios/SOQET/Debugging/Debug.cs
@@ -1,9 +1,19 @@
 namespace SOQET.Debugging
 {
+    /// <summary>
+    /// For Inspector Logging 
+    /// </summary>
     public static class Debug
     {
+        /// <summary>
+        /// Toggle Debug on/off
+        /// </summary>
         public static bool EnableDebug { get; set; } = true;
 
+        /// <summary>
+        /// log to console
+        /// </summary>
+        /// <param name="message"> information to display in inspector</param>
         public static void Log(string message)
         {
             if (!EnableDebug)
@@ -14,6 +24,10 @@ namespace SOQET.Debugging
             UnityEngine.Debug.Log(message);
         }
 
+        /// <summary>
+        /// Log a warning to console
+        /// </summary>
+        /// <param name="message">warning to display in inspector</param>
         public static void LogWarning(string message)
         {
             if (!EnableDebug)
@@ -24,6 +38,10 @@ namespace SOQET.Debugging
             UnityEngine.Debug.LogWarning(message);
         }
 
+        /// <summary>
+        /// Log an error to console
+        /// </summary>
+        /// <param name="message">error to display in inspector</param>
         public static void LogError(string message)
         {
             if (!EnableDebug)
