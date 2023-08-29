1477a17f7aa6b400054d771b68d5b508d14a7862
Gabriel Alabi <Rexes_Games@yahoo.com>
2023-08-29 11:16:06 +0100

    Add Sample Scene 2

---
+++ SOQETDevelopmentAndExample/Assets/Sweet Home Studios/SOQET/Extras/Scripts/MouseLook.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+
+     void Start()
+    {
+        Rigidbody body = GetComponent<Rigidbody>();
+        if (body != null)
+        {
+            body.freezeRotation = true;
+        }
+    }
+    public enum RotationAxis
+    {
+        MouseXAndY = 0,
+        MouseX = 1,
+        MouseY = 2
+    }
+
+    public RotationAxis axes = RotationAxis.MouseXAndY;
+
+    //sensitivity or speed variables
+    private float sensivityHor = 3.0f;
+    private float sensitivityVert = 3.0f;
+
+    //variables to control the limitations for vertical rotation
+    private float minimumVert = -45.0f;
+    private float maximumVert = 45.0f;
+
+    // variable for the vertical angle
+    private float _rotationX = 0;
+
+    void Update()
+    {
+        if (axes == RotationAxis.MouseX)                                                        // to check for horizontal rotation
+        {
+            // Code for horizontal rotation
+            transform.Rotate(0f, Input.GetAxis("Mouse X")* sensivityHor,  0f, Space.World);
+        }
+
+        else if (axes == RotationAxis.MouseY)                                                   // to check for vertical rotation 
+        {
+            // Code for vertical rotation
+            _rotationX -= Input.GetAxis("Mouse Y") * sensitivityVert;
+            _rotationX = Mathf.Clamp(_rotationX, minimumVert, maximumVert);
+
+            // variable to keep the same horizontal rotation
+            float rotationY = transform.localEulerAngles.y;
+            transform.localEulerAngles = new Vector3 (_rotationX, rotationY, 0);
+        }
+
+        else                                                                                   // to check for vertical  and horizontal  rotation 
+        {
+            // Code for vertical and horizontal rotation or rotation in the z-axis
+            transform.Rotate(0f, Input.GetAxis("Mouse X") * sensivityHor, 0f, Space.World);
+
+            _rotationX -= Input.GetAxis("Mouse Y") * sensitivityVert;
+            _rotationX = Mathf.Clamp(_rotationX, minimumVert, maximumVert);
+
+            // variable to keep the same horizontal rotation
+            float rotationY = transform.localEulerAngles.y;
+            transform.localEulerAngles = new Vector3(_rotationX, rotationY, 0);
+        }
+    }
+
+    public void Deactivate()
+    {
+        enabled = false;
+    }
+
+
+    public void Activate()
+    {
+        enabled = false;
+    }
+}
---
+++ SOQETDevelopmentAndExample/Assets/Sweet Home Studios/SOQET/Extras/Scripts/PlayerMovement.cs
@@ -0,0 +1,104 @@
+﻿using SOQET.Inspector;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+[RequireComponent(typeof(CharacterController))]
+[AddComponentMenu("Player Movement / FPS Input")]
+public class PlayerMovement : MonoBehaviour {
+
+    //variables to control movement speed
+    private float speed = 6;
+    private float gravity = -9.8f;
+
+    //to complete tutorial
+    private bool horizontalCompleted = false;
+    private bool verticalCompleted = false;
+    private bool movementTutorialCompleted = false;
+
+    //reference variable
+    private CharacterController _charController;
+    private MouseLook mouseLook;
+    [SerializeField] private SOQET.Inspector.SoqetInspector soqetInspector;
+
+    private void OnEnable()
+    {
+        //subscribe to the Press the directional keys to move "On Start Event"
+        soqetInspector.SubscribeToQuestOnStartEvent("Complete the Tutorial", "Press the directional keys to move", DisableMouseLook);
+
+        //subscribe to the Press the directional keys to move "On Complete Event"
+        soqetInspector.SubscribeToQuestOnCompleteEvent("Complete the Tutorial", "Press the directional keys to move", OnCompleteMoveTutorial);
+    }
+
+    private void OnDisable()
+    {
+        //unsubscribe from the Press the directional keys to move quest to prevent memory leaks
+        soqetInspector.UnsubscribeFromQuestOnCompleteEvent("Complete the Tutorial", "Press the directional keys to move", OnCompleteMoveTutorial);
+    }
+
+    private void OnCompleteMoveTutorial()
+    {
+        //On Complete Move Tutorial do something
+        Debug.Log("Completed Tutorial");
+
+        //Anything you want can be here
+        //Save game data
+        //give player an achievement
+        //gift player an item
+    }
+
+    private void DisableMouseLook()
+    {
+        //Prevent mouse look
+        Debug.Log("Completed Tutorial");
+        mouseLook.Deactivate();
+    }
+
+    void Start()
+    {
+        _charController = GetComponent<CharacterController>();
+        mouseLook = GetComponent<MouseLook>();
+    }
+
+	void FixedUpdate()
+    {
+        // to handle player movement along the x and z axis
+        float moveHorizontal = Input.GetAxis("Horizontal") * speed;
+        float moveVertical = Input.GetAxis("Vertical") * speed ;
+        Vector3 movement = new Vector3(moveHorizontal  ,0f  ,moveVertical);
+        
+        // to limit diagonal movement speed to the same speed as movement along an axis 
+        movement = Vector3.ClampMagnitude(movement, speed);
+        movement.y = gravity;
+
+        // To become Framerate independent
+        movement *= Time.deltaTime;
+
+        //to transform the vector from local to global coordinates
+        movement = transform.TransformDirection(movement);
+
+        //to move the character
+        _charController.Move(movement);
+
+        //complete tutorial
+        if (moveHorizontal > 0f)
+        {
+            horizontalCompleted = true;
+        }
+
+        if (moveVertical > 0f)
+        {
+            verticalCompleted = true;
+        }
+
+        if (!movementTutorialCompleted && horizontalCompleted && verticalCompleted)
+        {
+            movementTutorialCompleted = true;
+            soqetInspector.CompletePlayerQuest("Complete the Tutorial", "Press the directional keys to move");
+        }
+    }
+
+}
